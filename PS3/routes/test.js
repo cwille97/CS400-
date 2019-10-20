3b5ae2decc3cb131ca5a74b0065739c928b1678f
cwille97 <cwille97@gmail.com>
2019-10-20 00:12:48 -0400

    progress

--- PS3/routes/test.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.route('/')
-    .get(function (req, res, next) {
-        res.render('index', {title: 'This is a Test GET' }); // render a page with the GET information
-    });
-
-module.exports = router;
